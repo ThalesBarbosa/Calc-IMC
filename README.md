892563ff0af87a3555be313cacb11e61e9e55802
Thales Barbosa <thalesgx@hotmail.com>
2022-02-09 23:41:54 -0300

    calculadora de IMC com tela pre montada e js iniciado

---
+++ README.md
@@ -0,0 +1,3 @@
+Ezio Cintra de Lima 2101907
+Matheus Henrique da Silva 2102855
+Thales Barbosa Dantas 2102909
---
+++ script.js
@@ -0,0 +1,16 @@
+function calcular(altura, peso) {
+    imc = peso / (altura * altura);
+    return imc;
+  }
+  
+  function exibir(imc) {
+    if ((imc < 18, 5)) {
+      print("Magreza");
+    } else if ((imc >= 18, 5 && imc < 24, 9)) {
+      print("Normal");
+    } else if ((imc >= 24, 9 && imc < 30)) {
+      print("Sobrepeso");
+    } else if (imc >= 30) {
+      print("Obesidade");
+    }
+  }
